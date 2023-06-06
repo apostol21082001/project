b21d19daf8d9a0ee7cec56f2b91f1e7ef6ae1622
Mihail Apostol <81845771+apostol21082001@users.noreply.github.com>
2023-06-06 11:20:20 +0300

    Update System.js

--- System.js
+++ System.js
@@ -1,22 +1,51 @@
-import { SubscriptionSystem } from "./SubscriptionSystem.js";
-import { SubscriptionFactory } from "./SubscriptionFactory.js";
+import readline from 'readline';
+import { SubscriptionSystem } from './SubscriptionSystem.js';
+import { SubscriptionFactory } from './SubscriptionFactory.js';
+
 export class System {
     constructor() {
         this.subscriptionSystem = new SubscriptionSystem();
         this.subscriptionFactory = new SubscriptionFactory();
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
     }
 
-    sellSubscription(type, duration, price, isStudent) {
-        let actualPrice = price;
-        if (isStudent) {
-            //20% disc
-            actualPrice = price * 0.8;
-        }
-
-        const subscription = this.subscriptionFactory.createSubscription(type, duration, actualPrice);
-        this.subscriptionSystem.addSubscription(subscription);
-
-        console.log(`Sold a ${type} subscription for ${duration} months at $${actualPrice}`);
+    startSelling() {
+        console.log('|Start selling subscriptions|');
+        this.sellSubscription();
     }
-}// Facade Pattern
+
+    sellSubscription() {
+        this.rl.question('Type of subscription (or type /stop to exit): ', (type) => {
+            if (type === '/stop') {
+                this.rl.close();
+                return;
+            }
+
+            this.rl.question('Duration (in months): ', (duration) => {
+                this.rl.question('Price: ', (price) => {
+                    this.rl.question('Is the buyer a student? (yes/no): ', (isStudent) => {
+                        this.rl.question('Buyer Name: ', (buyerName) => {
+                            let actualPrice = parseFloat(price);
+                            if (isStudent.toLowerCase() === 'yes') {
+                                // 20% discount for students
+                                actualPrice *= 0.8;
+                            }
+
+                            const subscription = this.subscriptionFactory.createSubscription(type, parseInt(duration), actualPrice);
+                            subscription.buyerName = buyerName;
+                            this.subscriptionSystem.addSubscription(subscription);
+
+                            console.log(`Sold a ${type} subscription for ${duration} months at $${actualPrice} to ${buyerName}`);
+                            this.sellSubscription(); // Allow selling another subscription
+                        });
+                    });
+                });
+            });
+        });
+    }
+}
+// Facade Pattern
 
