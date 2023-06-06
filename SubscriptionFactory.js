c7c073b23e695f9277110caa3c2f2344bb99a5a2
Mihail Apostol <81845771+apostol21082001@users.noreply.github.com>
2023-06-06 11:19:32 +0300

    Update SubscriptionFactory.js

--- SubscriptionFactory.js
+++ SubscriptionFactory.js
@@ -1,8 +1,62 @@
-import { SubscriptionPrototype } from "./SubscriptionPrototype.js";
 export class SubscriptionFactory {
     createSubscription(type, duration, price) {
-        return new SubscriptionPrototype(type, duration, price);
-    }
-}// Factory Method Pattern
+        let subscription;
 
-//sub - >
+        switch (type) {
+            case 'Basic':
+                subscription = new BasicSubscription(duration, price);
+                break;
+            case 'Ultimate':
+                subscription = new UltimateSubscription(duration, price);
+                break;
+            case 'Premium':
+                subscription = new PremiumSubscription(duration, price);
+                break;
+            default:
+                throw new Error('Invalid subscription type');
+        }
+
+        return subscription;
+    }
+}
+
+export class Subscription {
+    constructor(duration, price) {
+        this.duration = duration;
+        this.price = price;
+        this.buyerName = '';
+    }
+
+    calculatePrice() {
+        return this.duration * this.price;
+    }
+
+    printDetails() {
+        console.log(`Type: ${this.constructor.name}`);
+        console.log(`Duration: ${this.duration} months`);
+        console.log(`Price: $${this.calculatePrice()}`);
+        console.log(`Buyer Name: ${this.buyerName}`);
+        console.log('-------------------------------------');
+    }
+}
+
+export class BasicSubscription extends Subscription {
+    constructor(duration, price) {
+        super(duration, price);
+        this.maxBooks = 1000;
+    }
+}
+
+export class UltimateSubscription extends Subscription {
+    constructor(duration, price) {
+        super(duration, price);
+        this.maxBooks = 2000;
+    }
+}
+
+export class PremiumSubscription extends Subscription {
+    constructor(duration, price) {
+        super(duration, price);
+        this.maxBooks = 5000;
+    }
+}
